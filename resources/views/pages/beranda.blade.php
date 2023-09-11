7ee4b30d50d50ef10ede820a386571e55b4e80d2
andi9g <andirizky.bayuputra@gmail.com>
2023-09-11 20:57:09 +0700

    oke

--- resources/views/pages/beranda.blade.php
+++ resources/views/pages/beranda.blade.php
@@ -25,7 +25,7 @@
         @foreach ($produk as $item)
         <div class="col-md-4">
             <div class="card" style="width: 18rem;">
-                <img src="{{ url('gambar/produk', [$item->gambar]) }}" class="card-img-top" alt="...">
+                <img src="{{ url('gambar/produk', [$item->gambar]) }}" class="card-img-top" style="max-height: 450px" alt="...">
                 <div class="card-body">
                   <small class="badge badge-secondary">{{ $item->kategori->namakategori }}</small>
                     <h4><a href="{{ route('detail.produk', [$item->idproduk]) }}" class="link pl-0 ml-0" style="text-decoration: none">{{ $item->namaproduk }}</a></h4>
